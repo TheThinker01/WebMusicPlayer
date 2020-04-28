2cce60d04c6640a6dd4157f626688b4d564946a4
TheThinker01 <archisman.pathak@iitkgp.ac.in>
2020-04-28 20:52:33 +0530

    added all buttons

--- js/main.js
+++ js/main.js
@@ -55,5 +55,46 @@ $('#stop').click(function(){
     audio.currentTime = 0;
 });
 
+// Next Button
+$('#next').click(function(){
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    if(next.length==0)
+    {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+    audio.play();
+    showDuration();
+});
+
+// Prev Button
+$('#prev').click(function(){
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    if(prev.length==0)
+    {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+    audio.play();
+    showDuration();
+});
+
+// Playlist Song change
+$('#playlist li').click(function(){
+    audio.pause();
+    initAudio($(this));
+    $('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn(400);
+    audio.play();
+    showDuration();
+});
+
+// Volume Control
+$('#volume').change(function(){
+    audio.volume = parseFloat(this.value/10);
+});
 
 
